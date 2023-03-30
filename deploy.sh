44d1d88d026612736af750c2f4700eead926d8a2
robertkigobe <robertkigobe@hotmail.com>
2023-03-30 02:29:50 -0400

    Comment

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+echo "Updating Git"
+git add .
+git commit -a -m "Comment"
+git push -u origin main
+
+echo "Building app"
+npm run build 
+
+echo "Deploying to server"
+scp -r build/* rkigobe@134.209.70.116:/var/www/134.209.70.116
+
+echo "done"
