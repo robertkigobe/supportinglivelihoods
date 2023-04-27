7e5f4a90d901769f2a88f86c200f2d9294ce8ba3
robertkigobe <robertkigobe@hotmail.com>
2023-04-27 06:36:13 -0400

    Dropdown Nav

--- src/components/Navigation/MainNav.jsx
+++ src/components/Navigation/MainNav.jsx
@@ -9,68 +9,69 @@ const MainNav = () => {
 
   const clickHandler = () => {
     setIsOpen((isOpen) => !isOpen);
-    
   };
 
   return (
-  
-      <header className="header">
-        
-        <div className="main-header">
-          <div className="main-header__toggle">
-            <button className="toggle-button" onClick={clickHandler}>
-              <span className="toggle-button__bar"></span>
-              UCCB
-            </button>
-          </div>
-          <div className="main-header__badge">
-            <img src={logo} alt="UCCB" className="logo__image" />
-          </div>
-
-          <div className="main-nav">
-            <ul className="main-nav__items">
-              <li className="main-nav__item">
-                <Link className="link" to="/">
-                  Who we are
-                </Link>
-              </li>
-              <li className="main-nav__item">
-                <Link className="link" to="/chaplaincy">
-                  How we Evangelize
-                </Link>
-              </li>
-              <li className="main-nav__item">
-                <Link className="link" to="/currentreading">
-                  Who we serve
-                </Link>
-              </li>
-              <li className="main-nav__item">
-                <Link className="link" to="/">
-                  Leadership
-                </Link>
-              </li>
-              <li className="main-nav__item">
-                <Link className="link" to="/">
-                  Ways to give
-                </Link>
-              </li>
-
-              <li className="main-nav__item">
-                <Link className="link" to="/">
-                  News
-                </Link>
-              </li>
-
-              <li className="main-nav__item main-nav__item--cta">
-                <Link className="link__cta" to="/">
-                  Donate
-                </Link>
-              </li>
-            </ul>
-          </div>
+    <header className="header">
+      <div className="main-header">
+        <div className="main-header__toggle">
+          <button className="toggle-button" onClick={clickHandler}>
+            <span className="toggle-button__bar"></span>
+            UCCB
+          </button>
         </div>
+        <div className="main-header__badge">
+          <img src={logo} alt="UCCB" className="logo__image" />
+        </div>
+
+        <div className="main-nav">
+          <ul className="main-nav__items">
+          <div className="main-nav__item">
+              <div className="main-nav__item__dropdown__head">Who we are </div>
+              <ul className="main-nav__item__dropdown_content">
+                <li>About Us</li>
+                <li>Leadership</li>
+                <li>President's Message</li>
+                <li>Chairman's Message</li>
+                <li>Executive Committee</li>
+                <li>Get in touch</li>
+              </ul>
+            </div>
+            <li className="main-nav__item">
+              <Link className="link" to="/chaplaincy">
+                How we Evangelize
+              </Link>
+            </li>
+            <li className="main-nav__item">
+              <Link className="link" to="/currentreading">
+                Who we serve
+              </Link>
+            </li>
+            
+            <li className="main-nav__item">
+              <Link className="link" to="/">
+                Ways to give
+              </Link>
+            </li>
+
+            <div className="main-nav__item">
+              <div className="main-nav__item__dropdown__head">News </div>
+              <ul className="main-nav__item__dropdown_content">
+                <li>Announcements</li>
+                <li>Just Recently</li>
+              </ul>
+            </div>
+
+            <li className="main-nav__item main-nav__item--cta">
+              <Link className="link__cta" to="/">
+                Donate
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
       <nav
-      onClick={clickHandler}
+        onClick={clickHandler}
         className="mobile-nav"
         style={{
           display: isOpen ? "block" : "none",
@@ -99,8 +100,7 @@ const MainNav = () => {
           </li>
         </ul>
       </nav>
-      </header>
-    
+    </header>
   );
 };
 export default MainNav;
